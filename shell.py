12789c2371b40fba91516626be90616827ea6e33
eSKa-0 <166024351+eSKa-0@users.noreply.github.com>
2025-01-15 23:39:30 +0100

    Add files via upload

--- shell.py
+++ shell.py
@@ -1,11 +1,10 @@
 import os
-from colorama import Fore
 
 def get_command():
-    return input(Fore.MAGENTA + ">>> " + Fore.RESET)
+    return input(">>> ")
 
 while True:
     try:
         os.system(get_command())
     except Exception as e:
-        print(Fore.RED + e + Fore.RESET)
+        print(e)
